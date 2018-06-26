ad6e3fe1f380d421f2ca801d98cd601e8193b516
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-06-26 15:55:22 +0800

    Changed test and added more options

--- circular_buffer_allocator/main.cpp
+++ circular_buffer_allocator/main.cpp
@@ -2,15 +2,22 @@
 #include <vector>
 #include "profiler.hpp"
 #include "circular_buffer_allocator.hpp"
+#define USE_CUSTOM_ALLOCATOR
 
 using zachariahs_world::custom_allocators::circular_buffer_allocator_type;
 
 template<typename type>
+#if defined USE_CUSTOM_ALLOCATOR
 using array_type = std::vector<type, circular_buffer_allocator_type<type>>;
+#else
+using array_type = std::vector<type>;
+#endif
 
 int main ( )
 {
 	constexpr auto num_tests = 1000;
+	constexpr auto num_ints = 10'000;
+	constexpr auto num_frames = 1000;
 
 	profiler my_profiler;
 
@@ -18,21 +25,36 @@ int main ( )
 	std::system ( "pause" );
 	std::cout << '\n';
 
+	auto test = [ num_ints, num_frames ]
+	{
+		auto ints = array_type<int> { };
+
+		for ( auto i = int { }; i < num_ints; ++i )
+		{
+			ints.push_back ( i );
+		}
+
+		for ( auto i = std::size_t { }; i < num_frames; ++i )
+		{
+			ints = [ &ints ]
+			{
+				auto results = array_type<int> { };
+
+				for ( const auto integer : ints )
+				{
+					results.push_back ( integer + 1 );
+				}
+
+				return results;
+			} ( );
+		}
+	};
+
+	// Warmup
+	test ( );
+
 	for ( auto i = std::size_t { }; i < num_tests; ++i )
 	{
-		auto test = [ ]
-		{
-			auto array = array_type<int> { };
-
-			for ( auto i = int { }; i < 10'000; ++i )
-			{
-				array.push_back ( i );
-			}
-		};
-
-		// Warmup
-		test ( );
-
 		// Beginning of profile
 		my_profiler.start ( );
 		test ( );
@@ -43,6 +65,9 @@ int main ( )
 	std::cout << "Programmed with: "
 #if defined _WIN64
 		<< "[_WIN64]"
+#endif
+#if defined USE_CUSTOM_ALLOCATOR
+		<< "[USE_CUSTOM_ALLOCATOR]"
 #endif
 		<< '\n';
 	std::cout << "Average: " << my_profile.mean << '\n';
